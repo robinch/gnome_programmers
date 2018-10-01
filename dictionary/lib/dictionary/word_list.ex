69aa2e561c2ad88712b71d56bc850090b7494158
Robin Chowdhury <robinch88@gmail.com>
2018-10-01 17:46:11 +0200

    Dictionary now uses Supervisor

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -2,7 +2,7 @@ defmodule Dictionary.WordList do
 
   @me __MODULE__
   def start_link() do
-    Agent.start(&word_list/0, name: @me )
+    Agent.start_link(&word_list/0, name: @me )
   end
 
   def random_word() do
