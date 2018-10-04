efdb7c4264027ca98dc057c85d7bd771e46aaab2
Robin Chowdhury <robinch88@gmail.com>
2018-10-04 16:39:17 +0200

    Created Chain script that tests several nodes

---
+++ nodes/chain.exs
@@ -0,0 +1,24 @@
+defmodule Chain do
+  defstruct(
+    next_node: nil,
+    count: 10000
+  )
+
+  def start_link(next_node) do
+    spawn_link(Chain, :message_loop, [%Chain{next_node: next_node}])
+    |> Process.register(:chainer)
+  end
+
+  def message_loop(%Chain{count: 0}) do
+    IO.puts("done")
+  end
+
+  def message_loop(state) do
+    receive do
+      {:trigger, list} ->
+        send({:chainer, state.next_node}, {:trigger, [node() | list]})
+    end
+
+    message_loop(%Chain{state | count: state.count - 1})
+  end
+end
